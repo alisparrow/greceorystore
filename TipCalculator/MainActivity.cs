769979896f7c8309aaab8bf481a616d0e811fcb6
idsystem <idsystem@ASUSK551>
2017-03-24 12:03:36 +0430

    Add project files.

---
+++ TipCalculator/MainActivity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TipCalculator
+{
+    [Activity(Theme = "@android:style/Theme.Material.Light.DarkActionBar", Label = "TipCalculator", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+       EditText inputBill;
+        TextView outputTip;
+        TextView outputTotal;
+        Button calculateButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+            calculateButton.Click += CalculateButton_Click;
+        }
+
+         void CalculateButton_Click(object sender, EventArgs e)
+        {
+           var text = inputBill.Text;
+            komisiyonmelk.komesiyon c = new komisiyonmelk.komesiyon();
+            c.TotallKomesiyone = UInt64.Parse(text);
+            if (c.TotallKomesiyone <= komisiyonmelk.komesiyon.Sadmilion)
+                c.PlanOne();
+            else if (c.TotallKomesiyone <= komisiyonmelk.komesiyon.Divestmilion)
+            {
+                c.PlanTow();
+                c.PlanOne();
+            }
+            else if (c.TotallKomesiyone <= komisiyonmelk.komesiyon.Divestmilion)
+            {
+
+                c.PlanThree();
+                c.PlanOne();
+                c.PlanTow();
+            }
+            else
+            {
+                c.PlanFour();
+                c.PlanOne();
+                c.PlanTow();
+                c.PlanThree();
+            }
+            outputTip.Text = c.Streamoutput.ToString();
+
+
+        }
+    }
+}
+
---
+++ TipCalculator/komisiyonmelk.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace komisiyonmelk
+{
+    class komesiyon
+    {
+        //filed hai vase class mored nazar
+      
+       private  double KomesiyoneDoTaraf;
+     //constactor vase inke meghdar komesiyone ro begirim
+     public komesiyon() {
+
+            this.KomesiyoneDoTaraf = 0;
+        }
+
+        //feild hai public ma
+             public  ulong TotallKomesiyone;
+            public const uint Sadmilion = 100000000;
+         public const uint Divestmilion = 200000000;
+        public const uint Divestopanjahmilion = 250000000;
+        //metod hai ke estefadeh meshan
+        public void PlanOne()
+        {
+            if (TotallKomesiyone <= Sadmilion)
+               KomesiyoneDoTaraf +=TotallKomesiyone * 0.01;
+            else
+                KomesiyoneDoTaraf += Sadmilion * 0.01;
+        }
+        public void PlanTow()
+        {
+            if (TotallKomesiyone <= Divestmilion)
+               KomesiyoneDoTaraf += (Divestmilion - TotallKomesiyone) * 0.007;
+            else
+               KomesiyoneDoTaraf += (Divestmilion - Sadmilion) * 0.007;
+        }
+        public void PlanThree()
+        {
+            if (TotallKomesiyone <= Divestopanjahmilion)
+               KomesiyoneDoTaraf += (Divestopanjahmilion - TotallKomesiyone) * 0.005;
+            else
+               KomesiyoneDoTaraf += ((Sadmilion + Divestmilion) - Divestopanjahmilion) * 0.005;
+        }
+        public void PlanFour()
+        {
+           KomesiyoneDoTaraf += (TotallKomesiyone- Divestopanjahmilion) * 0.0025;
+        }
+        public double Streamoutput
+        {
+            get { return KomesiyoneDoTaraf; }
+        }
+    }
+    }
+
+
